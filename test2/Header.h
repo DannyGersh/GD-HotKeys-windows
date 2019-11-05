f0e11de73c12ac322bbdf0f77a217bda3009828c
dannygershman96 <dannygershman96@gmail.com>
2019-11-05 16:42:31 +0200

    first commit

---
+++ test2/Header.h
@@ -0,0 +1,123 @@
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+#include <vector>
+
+
+int ID_nextHK = 100;
+
+class HK : public wxWindow {
+private:
+	wxCheckBox* CheckBox;
+	struct c {
+		wxComboBox* key; // keyboard key
+		wxComboBox* mod; // ctrl / alt / none
+		wxComboBox* exe; // path to executible
+		wxComboBox* vis; // will the window be visible
+		wxComboBox* arg; // path to the file to be executed, with args.
+	} C;
+	wxButton* folderBTN;
+	wxButton* delBTN;
+public:
+	wxBoxSizer* vbox = new wxBoxSizer(wxHORIZONTAL);
+public:
+	HK(wxWindow* parent, wxWindowID id);
+	void OnCheckBox(wxCommandEvent& event);
+	void OnKey(wxCommandEvent& event);
+	void OnMod(wxCommandEvent& event);
+	void OnExe(wxCommandEvent& event);
+	void OnVis(wxCommandEvent& event);
+	void OnArg(wxCommandEvent& event);
+	void OnFolderBTN(wxCommandEvent& event);
+	void OnDelBTN(wxCommandEvent& event);
+};
+
+class scrollWND : public wxScrolledWindow
+{
+public:
+	wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+
+	//struct HK {
+	//	wxCheckBox* CheckBox;
+	//
+	//	struct c {
+	//		wxComboBox* key; // keyboard key
+	//		wxComboBox* mod; // ctrl / alt / none
+	//		wxComboBox* exe; // path to executible
+	//		wxComboBox* vis; // will the window be visible
+	//		wxComboBox* arg; // path to the file to be executed, with args.
+	//	} C;
+	//
+	//	wxButton* folderBTN;
+	//	wxButton* delBTN;
+	//
+	//	int id;
+	//	int index;
+	//};
+	//std::vector<HK> HKs;
+
+public:
+	scrollWND(wxWindow* parent, wxWindowID id);
+	void newHK();
+	void poop(wxCommandEvent& event);
+
+	void OnCheckBox(wxCommandEvent& event);
+	void OnKey(wxCommandEvent& event);
+	void OnMod(wxCommandEvent& event);
+	void OnExe(wxCommandEvent& event);
+	void OnVis(wxCommandEvent& event);
+	void OnArg(wxCommandEvent& event);
+	void OnFolderBTN(wxCommandEvent& event);
+	void OnDelBTN(wxCommandEvent& event);
+
+};
+
+
+
+class MyApp : public wxApp
+{
+public:
+	virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame
+{
+public:
+	scrollWND* scrollwnd;
+	wxBoxSizer* sizer;
+
+public:
+	MyFrame();
+private:
+	void OnHello(wxCommandEvent& event);
+	void OnExit(wxCommandEvent& event);
+	void OnAbout(wxCommandEvent& event);
+	
+	void newHK(wxCommandEvent& event);
+
+	wxDECLARE_EVENT_TABLE();
+
+};
+
+
+
+
+enum
+{
+	ID_Hello = 1,
+	ID_newHKbtn = 2,
+	ID_test = 12345
+};
+
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+EVT_MENU(ID_Hello, MyFrame::OnHello)
+EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+EVT_BUTTON(ID_newHKbtn, MyFrame::newHK)
+wxEND_EVENT_TABLE()
+
+
+
+
+
---
+++ test2/Source.cpp
@@ -0,0 +1,214 @@
+#include "wx/wxprec.h"
+#include "Header.h"
+
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+	MyFrame *frame = new MyFrame();
+	frame->SetSize(800, 800);
+	frame->Show(true);
+	return true;
+}
+
+
+HK::HK(wxWindow* parent, wxWindowID id) : wxWindow(parent, id)
+{
+	CheckBox = new wxCheckBox(this, ID_nextHK, "");
+	C = {
+		new wxComboBox(this, ID_nextHK + 1, std::to_string(ID_nextHK + 1)),
+		new wxComboBox(this, ID_nextHK + 2, std::to_string(ID_nextHK + 2)),
+	    new wxComboBox(this, ID_nextHK + 3, std::to_string(ID_nextHK + 3)),
+	    new wxComboBox(this, ID_nextHK + 4, std::to_string(ID_nextHK + 4)),
+	    new wxComboBox(this, ID_nextHK + 5, std::to_string(ID_nextHK + 5)),
+	};
+	folderBTN = new wxButton(this, ID_nextHK + 6, "6");
+	delBTN = new wxButton(this, ID_nextHK + 7, "7");
+
+	// checkbox
+	{
+		CheckBox->SetValue(true);
+		vbox->Add(CheckBox, 0, wxALL, 2);
+	}
+	 
+	// comboboxes
+	{
+		vbox->Add(C.key, 0, wxALL, 1);
+		vbox->Add(C.mod, 0, wxALL, 2);
+		vbox->Add(C.exe, 0, wxALL, 2);
+		vbox->Add(C.vis, 0, wxALL, 2);
+		vbox->Add(C.arg, 1, wxALL, 2);
+	}
+
+	// buttons
+	{
+		folderBTN->SetMinSize({ folderBTN->GetSize().y, folderBTN->GetSize().y });
+		delBTN->SetMinSize({ delBTN->GetSize().y, delBTN->GetSize().y });
+		vbox->Add(folderBTN, 0, wxALL, 2);
+		vbox->Add(delBTN, 0, wxALL, 2);
+	}
+
+	this->SetSizer(vbox);
+	this->FitInside();
+
+	Bind(wxEVT_CHECKBOX, &HK::OnCheckBox, this, ID_nextHK);
+	Bind(wxEVT_TEXT, &HK::OnCheckBox, this, ID_nextHK + 1);
+	Bind(wxEVT_TEXT, &HK::OnCheckBox, this, ID_nextHK + 2);
+	Bind(wxEVT_TEXT, &HK::OnCheckBox, this, ID_nextHK + 3);
+	Bind(wxEVT_TEXT, &HK::OnCheckBox, this, ID_nextHK + 4);
+	Bind(wxEVT_TEXT, &HK::OnCheckBox, this, ID_nextHK + 5);
+	Bind(wxEVT_BUTTON, &HK::OnCheckBox, this, ID_nextHK + 6);
+	Bind(wxEVT_BUTTON, &HK::OnCheckBox, this, ID_nextHK + 7);
+	ID_nextHK += 8;
+}
+void HK::OnCheckBox(wxCommandEvent& event)
+{
+	wxMessageBox("HK::OnCheckBox()");
+	this->C.arg->Enable(false);
+}
+
+void HK::OnKey(wxCommandEvent& event)
+{
+}
+void HK::OnMod(wxCommandEvent& event)
+{
+}
+void HK::OnExe(wxCommandEvent& event)
+{
+}
+void HK::OnVis(wxCommandEvent& event)
+{
+}
+void HK::OnArg(wxCommandEvent& event)
+{
+}
+void HK::OnFolderBTN(wxCommandEvent& event)
+{
+}
+void HK::OnDelBTN(wxCommandEvent& event)
+{
+}
+
+
+
+scrollWND::scrollWND(wxWindow* parent, wxWindowID id)
+	: wxScrolledWindow(parent, id) 
+{}
+void scrollWND::poop(wxCommandEvent& event)
+{
+	wxMessageBox(std::to_string(event.GetId()));
+}
+void scrollWND::newHK() {
+	
+	HK* h = new HK(this, wxID_ANY);
+
+	sizer->Add(h->vbox, 0, wxEXPAND, 2);
+	this->SetSizer(sizer);
+	this->FitInside();
+	this->SetScrollRate(10, 10);
+
+	//Bind(wxEVT_CHECKBOX, &scrollWND::OnCheckBox, this, ID_nextHK);
+	//Bind(wxEVT_TEXT, &scrollWND::OnCheckBox, this, ID_nextHK + 1);
+	//Bind(wxEVT_TEXT, &scrollWND::OnCheckBox, this, ID_nextHK + 2);
+	//Bind(wxEVT_TEXT, &scrollWND::OnCheckBox, this, ID_nextHK + 3);
+	//Bind(wxEVT_TEXT, &scrollWND::OnCheckBox, this, ID_nextHK + 4);
+	//Bind(wxEVT_TEXT, &scrollWND::OnCheckBox, this, ID_nextHK + 5);
+	//Bind(wxEVT_BUTTON, &scrollWND::OnCheckBox, this, ID_nextHK + 6);
+	//Bind(wxEVT_BUTTON, &scrollWND::OnCheckBox, this, ID_nextHK + 7);
+	//ID_nextHK += 8;
+}
+
+
+void scrollWND::OnCheckBox(wxCommandEvent& event)
+{
+	int id = event.GetId();
+	wxMessageBox(std::to_string(id));
+	//HKs[0].C.arg->Enable(false);
+	FindWindowById(id);
+};
+void scrollWND::OnKey(wxCommandEvent& event)
+{
+};
+void scrollWND::OnMod(wxCommandEvent& event)
+{
+};
+void scrollWND::OnExe(wxCommandEvent& event)
+{
+};
+void scrollWND::OnVis(wxCommandEvent& event)
+{
+};
+void scrollWND::OnArg(wxCommandEvent& event)
+{
+};
+void scrollWND::OnFolderBTN(wxCommandEvent& event)
+{
+};
+void scrollWND::OnDelBTN(wxCommandEvent& event)
+{
+};
+
+
+
+
+MyFrame::MyFrame()
+	: wxFrame(NULL, wxID_ANY, "Hello World")
+{
+	wxMenu *menuFile = new wxMenu;
+	menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+		"Help string shown in status bar for this menu item");
+	menuFile->AppendSeparator();
+	menuFile->Append(wxID_EXIT);
+
+	wxMenu *menuHelp = new wxMenu;
+	menuHelp->Append(wxID_ABOUT);
+
+	wxMenuBar *menuBar = new wxMenuBar;
+	menuBar->Append(menuFile, "&File");
+	menuBar->Append(menuHelp, "&Help");
+	SetMenuBar(menuBar);
+	CreateStatusBar();
+	SetStatusText("Welcome to wxWidgets!");
+	Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+	Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+	Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+
+	scrollwnd = new scrollWND(this, wxID_ANY);
+	sizer = new wxBoxSizer(wxVERTICAL);
+
+	sizer->Add(scrollwnd, 1, wxEXPAND);
+
+	wxBoxSizer* hbox = new wxBoxSizer(wxHORIZONTAL);
+	hbox->Add(
+		new wxButton(this, ID_newHKbtn, "POOP"),
+		1, wxALL, 10
+	);
+	hbox->Add(
+		new wxButton(this, ID_newHKbtn, "POOP"),
+		1, wxALL, 10
+	);
+
+	sizer->Add(hbox, 0, wxEXPAND | wxBOTTOM);
+	this->SetSizer(sizer);
+	this->Show();
+}
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+	Close(true);
+}
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+	wxMessageBox("This is a wxWidgets Hello World example",
+		"About Hello World", wxOK | wxICON_INFORMATION);
+}
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+	wxLogMessage("Hello world from wxWidgets!");
+}
+void MyFrame::newHK(wxCommandEvent& event)
+{
+	this->scrollwnd->newHK();
+}
+
+
